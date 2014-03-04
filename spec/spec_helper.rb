29c9acb6a4c11a452abd8af3b9b10aa051b1a87e
Eric Loveland <eric@ericloveland.com>
2014-03-04 00:47:35 -0500

    initial add of decorate_icon.rbhttps://github.com/adamwaite/iOS-Build-Kit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,3 +9,31 @@ RSpec.configure do |config|
 
   config.order = 'random'
 end
+
+### From decorate_icon
+# require 'rake'
+#
+# $:.push File.expand_path("../lib", __FILE__)
+#
+# Dir["lib/ios_build_kit/**/*.rb"].reject { |file| file.include? "lib/ios_build_kit/version.rb" }.each { |f| load(f) }
+#
+# require_relative 'support/config_mocker.rb'
+# require_relative 'support/temp_dir.rb'
+#
+# RSpec.configure do |config|
+#
+#   config.color_enabled = true
+#   config.tty = true
+# 	config.formatter = :documentation
+# 	config.fail_fast = true
+#
+# 	config.before(:each) do
+#     create_spec_temp_dir
+#     clone_and_modify_icons
+#   end
+#
+# 	config.after(:each) do
+#     destroy_spec_temp_dir
+#   end
+#
+# end
